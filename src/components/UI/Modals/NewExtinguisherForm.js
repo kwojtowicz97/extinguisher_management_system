068e499d31ade07e29101b378e4709397b6082e8
Kamil Wójtowicz <kamilwojtowicz079@gmail.com>
2022-05-22 13:06:32 +0200

    check form validity + optional insp and ovhl date

--- src/components/UI/Modals/NewExtinguisherForm.js
+++ src/components/UI/Modals/NewExtinguisherForm.js
@@ -2,15 +2,18 @@ import { useState } from "react";
 import { addExtinguisher } from "../../../context/actions/extinguisher";
 import { useContext } from "react";
 import { appContext } from "../../../context/store/appContext";
+import { hideModal } from "../../../context/actions/ui";
 
 export const NewExtinguisherForm = () => {
   const appCtx = useContext(appContext);
-  const { extinguishersDispatch } = appCtx;
-  const [producerState, setProducer] = useState();
-  const [typeState, setType] = useState();
-  const [prodDateState, setProdDate] = useState();
-  const [inspDateState, setInspDate] = useState();
-  const [ovhlDateState, setOvhlDate] = useState();
+  const { extinguishersDispatch, modalDispatch } = appCtx;
+  const [producerState, setProducer] = useState("");
+  const [typeState, setType] = useState("");
+  const [prodDateState, setProdDate] = useState("");
+  const [inspDateState, setInspDate] = useState("");
+  const [inspDateCheckbox, setInspDateCheckbox] = useState(false)
+  const [ovhlDateState, setOvhlDate] = useState("");
+  const [ovhlDateCheckbox, setOvhlDateCheckbox] = useState(false);
 
   const producerChangeHandler = (e) => {
     setProducer(e.target.value);
@@ -34,8 +37,8 @@ export const NewExtinguisherForm = () => {
       producerState,
       typeState,
       prodDateState,
-      inspDateState,
-      ovhlDateState,
+      inspDateCheckbox && inspDateState,
+      ovhlDateCheckbox && ovhlDateState,
     ];
     if (data.some((element) => element === undefined)) return;
     const extinguisher = {
@@ -47,48 +50,75 @@ export const NewExtinguisherForm = () => {
     };
 
     extinguishersDispatch(addExtinguisher(extinguisher));
+    modalDispatch(hideModal());
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={addExtinguisherHandler}>
         <div>
           <label htmlFor="producer">Producer</label>
           <input
             id="producer"
             type="text"
+            value={producerState}
             onChange={producerChangeHandler}
+            required
           ></input>
         </div>
         <div>
           <label htmlFor="type">Type</label>
-          <input id="type" type="text" onChange={typeChangeHandler}></input>
+          <input
+            id="type"
+            type="text"
+            value={typeState}
+            onChange={typeChangeHandler}
+            required
+          ></input>
         </div>
         <div>
           <label htmlFor="production-date">Production date</label>
           <input
             id="production-date"
             type="date"
+            value={prodDateState}
             onChange={prodDateChangeHandler}
+            required
           ></input>
         </div>
         <div>
+          <input
+            type="checkbox"
+            checked={inspDateCheckbox}
+            onChange={() => setInspDateCheckbox((prev) => !prev)}
+          ></input>
           <label htmlFor="inspection-date">Inspection date</label>
           <input
             id="inspection-date"
             type="date"
+            value={inspDateState}
+            required={inspDateCheckbox}
+            disabled={!inspDateCheckbox}
             onChange={inspDateChangeHandler}
           ></input>
         </div>
         <div>
+          <input
+            type="checkbox"
+            checked={ovhlDateCheckbox}
+            onChange={() => setOvhlDateCheckbox((prev) => !prev)}
+          ></input>
           <label htmlFor="overhaul-date">Overhaul date</label>
           <input
             id="overhaul-date"
             type="date"
+            value={ovhlDateState}
+            required={ovhlDateCheckbox}
+            disabled={!ovhlDateCheckbox}
             onChange={ovhlDateChangeHandler}
           ></input>
         </div>
-        <button onClick={addExtinguisherHandler}>Add</button>
+        <button type="submit">Add</button>
       </form>
     </div>
   );
