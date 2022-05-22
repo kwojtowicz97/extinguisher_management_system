5b8d80600297f0dfc9b3d1b4d6b38e6b23e62e63
Kamil Wójtowicz <kamilwojtowicz079@gmail.com>
2022-05-22 13:48:04 +0200

    deleted list.js

--- src/components/Controls/lists/ExtinguishersList.js
+++ src/components/Controls/lists/ExtinguishersList.js
@@ -1,12 +1,30 @@
-import { useContext } from "react"
-import { appContext } from "../../../context/store/appContext"
+import { useContext, useState } from "react";
+import { appContext } from "../../../context/store/appContext";
 
 export const ExtinguishersList = () => {
-    const appCtx = useContext(appContext)
+  const appCtx = useContext(appContext);
+  const [sortByState, setSortByState] = useState("default")
 
-    const { extinguishersState } = appCtx;
+  const { extinguishersState } = appCtx;
 
-    return (<ul>
-        {extinguishersState.map(extinguisher => <li key={extinguisher.id}>{`${extinguisher.producer} ${extinguisher.type}`}</li>)}
-    </ul>)
-}
+  return (
+    <div>
+      <label for="sortOptions">Sort by</label>
+      <select id="sortOptions">
+        <option value="default" selected>Default</option>
+        <option value="producer">Producer</option>
+        <option value="type">Type</option>
+        <option value="productionDate">Production Date</option>
+        <option value="inspectionDate">Inspection Date</option>
+        <option value="overhaulDate">Overhaul Date</option>
+      </select>
+      <ul>
+        {extinguishersState.map((extinguisher) => (
+          <li
+            key={extinguisher.id}
+          >{`${extinguisher.producer} ${extinguisher.type}`}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
